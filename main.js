d8b0af8be5cbc4cc79e610012aa12accbcb5883a
Eugene Lim <eugenood@gmail.com>
2021-01-13 14:37:56 +0800

    Add dotenv

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const Database = require("./src/database");
 const Bot = require("./src/bot");
 const Server = require("./src/server")
 
+require("dotenv").config()
+
 const db = new Database("db.json");
 const bot = new Bot(process.env.BOT_TOKEN, db);
 const server = new Server(process.env.PORT, db);
